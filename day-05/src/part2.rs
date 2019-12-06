d81403b4458272a5f4c66eeb23d533f5f92ddef2
Patrick Bruckner <pbruckner22@gmail.com>
2019-12-05 23:04:11 -0600

    Finished day-05

---
+++ day-05/src/part2.rs
@@ -0,0 +1,16 @@
+use std::fs::read_to_string;
+use crate::computer::Computer;
+use crate::computer::Program;
+
+pub fn part2()
+{
+    let mut intcode_str = read_to_string("input/part1.txt").unwrap();
+    intcode_str = intcode_str.trim_end().to_string();
+
+    let program = Program::new_from_str(intcode_str);
+    let mut computer = Computer::new();
+    computer.load_program(program);
+
+    println!("Press 5 at the prompt");
+    computer.run();
+}
