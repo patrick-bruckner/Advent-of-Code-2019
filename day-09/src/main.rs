2a365fbba75260e20bfcfb9bb25ab78d6700106b
Patrick Bruckner <pbruckner22@gmail.com>
2019-12-09 20:37:15 -0600

    Finished day 09

---
+++ day-09/src/main.rs
@@ -0,0 +1,9 @@
+mod part1;
+mod part2;
+mod computer;
+
+fn main()
+{
+    part1::part1();
+    part2::part2();
+}
---
+++ day-09/src/part2.rs
@@ -0,0 +1,29 @@
+use std::fs::read_to_string;
+use crate::computer::{Computer, Program, IOQueue};
+
+pub fn part2()
+{
+    let mut intcode_str = read_to_string("input/part1.txt").unwrap();
+    intcode_str = intcode_str.trim_end().to_string();
+    let program = Program::new_from_str(intcode_str);
+
+    let mut computer = Computer::new();
+    computer.load_program(&program);
+    let computer_input = IOQueue::new();
+    let computer_output = IOQueue::new();
+    computer.set_input(&computer_input);
+    computer.set_output(&computer_output);
+
+    computer_input.push_front("2".to_string());
+    computer.run();
+
+    if !computer.is_halted()
+    {
+        panic!("Computer isn't finished");
+    }
+
+    for s in computer_output
+    {
+        println!("{}", s);
+    }
+}
